1561f01ebb5f8bdccb0e2501e318aac86a95f57d
Ddhhddjh <122529034+Ddhhddjh@users.noreply.github.com>
2023-03-17 06:47:58 +0000

    Update GROVER SCRIPT HUB.lua

--- GROVER SCRIPT HUB.lua
+++ GROVER SCRIPT HUB.lua
@@ -1,4 +1,4 @@
-9local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({Name = "GROVER SCRIPT HUB", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
